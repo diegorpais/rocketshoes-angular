3ca17e4eb8ab02e4bfad54a561be40ef10fddb24
Diego Pais <drborges.pais@gmail.com>
2022-06-12 12:57:06 -0300

    feat: getting products from a fake API

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ProductsService } from 'src/app/services/products.service';
+import { ProductModel } from 'src/app/models/product.model';
+import { formatPrice } from 'src/app/util/util';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  products: ProductModel[] = [];
+
+  constructor(
+    private productsService: ProductsService,
+  ) { }
 
   ngOnInit(): void {
+    this.getProductList();
+  }
+
+  getProductList() {
+    this.productsService.getProductsList().subscribe({
+      next: (res) => this.handleProduct(res),
+      error: (error) => console.log(error),
+    });
+  }
+
+  handleProduct(products: ProductModel[]) {
+    const data = products.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+    
+    this.products = data;
   }
 
 }
