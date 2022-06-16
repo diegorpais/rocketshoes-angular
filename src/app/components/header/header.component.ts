a457590ca17f32d416350e9dc858321c0c8e394d
Diego Pais <drborges.pais@gmail.com>
2022-06-16 10:49:05 -0300

    feat: showing total products in the header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Store, select } from '@ngrx/store';
+import { AppState } from 'src/app/store/reducers/index';
+import { Observable } from 'rxjs';
+
+import { ProductModel } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-header',
@@ -7,16 +12,30 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  products$: Observable<Array<ProductModel>>;
+  totalItens = 0;
 
   constructor(
     private router: Router,
-  ) { }
+    private store: Store<AppState>,
+  ) {
+    this.products$ = this.store.pipe(select('product'));
+  }
 
   ngOnInit(): void {
+    this.getTotalItens();
   }
 
   goTo(route: string) {
     this.router.navigate([route]);
+  }
+
+  getTotalItens() {
+    this.products$.subscribe(
+      res => {
+        this.totalItens = res.length;
+      },
+    );
   }
 
 }
